01aa33baf1e7a0bd9a5eeb0fd74ca328a8f4d736
swarnametals <sivakumar.shanmugam@plrprojects.com>
2024-12-19 11:54:26 +0100

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="icon" href="{{ asset('images/logo_circle.pg') }}" type="image/x-icon">
+  <link rel="icon" href="{{ asset('images/logo_circle.jpg') }}" type="image/x-icon">
   <title>@yield('title', 'Swarna Metals Zambia Limited (SMZL)')</title>
 
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
